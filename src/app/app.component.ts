4cd2dd98db04798b967db05974158c3fc490dbe8
Francisco Javier Florencio Airado <fjflorenc@gmail.com>
2024-06-26 14:44:41 +0200

    create header and user-input form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HeaderComponent, UserInputComponent],
 })
-export class AppComponent {
-  title = 'investment-calculator';
-}
+export class AppComponent {}
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule,],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '5';
+  duration = '0';
+
+  onSubmit() {
+    console.log(this.initialInvestment);
+    console.log(this.annualInvestment);
+    console.log(this.expectedReturn);
+    console.log(this.duration);
+  }
+
+}
